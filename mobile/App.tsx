0e96bfb300869f91ec639d6419a2bc7198604569
Rogério Rocha <neorogerio@gmail.com>
2022-11-07 13:02:50 -0300

    Aula 04 - Mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,25 @@
+import React from "react";
 import { NativeBaseProvider, StatusBar } from "native-base";
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { THEME } from './src/styles/theme'
-import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/Loading";
 import { AuthContextProvider } from "./src/context/AuthContext";
 
-import { Pools } from "./src/screens/Pools";
+import { Routes } from "./src/routes";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
 
-  return (  
+  return (
     <NativeBaseProvider theme={THEME}>
       <AuthContextProvider>
         <StatusBar
@@ -18,7 +27,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-          { fontsLoaded ? <Pools /> : <Loading /> }
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
